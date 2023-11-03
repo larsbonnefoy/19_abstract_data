4fe736a86f3407e95edb41ea0d9af0d424193a56
Lars Bonnefoy <lars.bonnefoy@hotmail.com>
2023-11-03 22:22:11 +0100

    some progress, need to figure out how memory allocation in vector works

--- Includes/Vector.hpp
+++ Includes/Vector.hpp
@@ -1,4 +1,6 @@
 #include <cstddef>
+#include <cstring>
+#include <exception>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -13,13 +15,35 @@ template<class T,class Allocator = std::allocator<T> > class vector {
     typedef std::ptrdiff_t                              difference_type;
     typedef typename allocator_type::reference          reference;
     typedef typename allocator_type::const_reference    const_reference;
+    typedef typename allocator_type::pointer            pointer;
     
 
     private:
-        T*              _data;
-        allocator_type  _alloc;
+        pointer         _data;
+        allocator_type  _allocator;
         size_type       _size;
         size_type       _capacity;
+
+        /**
+         * Returns most significant bit of number n  
+         * Used to get the next power closet power of 2 bigger than n to 
+         * pre-allocate memory in power of 2 blocks.
+         *
+         * @param n - number to find msb
+         *
+         * @returns size_type equivalent to the msb position
+         */
+        size_type msb(size_type n) {
+            size_type count = 0;
+            if (n == 0) {
+                return 0;
+            }
+            while (n != 0) {
+                n = n / 2;
+                count++;
+            }
+            return count;
+        }
 
     public:
 
@@ -31,18 +55,18 @@ template<class T,class Allocator = std::allocator<T> > class vector {
          * @return empty vector
          */
         vector( void ) 
-            : _data(0x0), _alloc(allocator_type()), _size(0), _capacity(0) {
+            : _data(0x0), _allocator(allocator_type()), _size(0), _capacity(0) {
         }
 
         /**
-         * Constructs an empty container with the given alloctor alloc
+         * Constructs an empty container with the given allocator alloc
          * Complexity: Constant 
          *
          * @param alloc - allocator used for memory allocation of this container
          * @return empty vector
          */
         explicit vector( const Allocator& alloc ) 
-            : _data(0x0), _alloc(alloc), _size(0), _capacity(0) {
+            : _data(0x0), _allocator(alloc), _size(0), _capacity(0) {
         }
         
         /**
@@ -54,10 +78,12 @@ template<class T,class Allocator = std::allocator<T> > class vector {
          * @param alloc - allocator used for memory allocation of this container
          *
          * @return initialized vector
+         *
          */
         explicit vector( size_type count, const T& value = T(), const Allocator& alloc = Allocator() ) 
-            : _alloc(alloc), _size(count) {
-
+            :  _allocator(alloc), _size(count), _capacity(count) {
+                _data = _allocator.allocate(_capacity);
+                assign(count, value);
         }
 
         /**
@@ -72,41 +98,138 @@ template<class T,class Allocator = std::allocator<T> > class vector {
         vector( const vector& other ) {
         }
 
+        /**
+         * Destructs the vector
+         */
+        ~vector( void ) {
+            for (size_type i = 0; i < _size; i++) {
+                _allocator.destroy(&_data[i]);
+            }
+            _allocator.deallocate(_data, _capacity);
+        }
+        
+        //TODO
         /* Iterator Constructor do to
          template< class InputIt >
             vector( InputIt first, InputIt last, const Allocator& alloc = Allocator() );
         */
 
-        /* ##Exceptions: std::allocator may throw
+
+        //TODO check if use Iterator to fill in the container
+        //TODO USE std::allocator.construct()?
+        /**
+         * Replaces the contents with count copies of value.\n
+         * Complexity: Linear in count
          *
+         * @param count - new size of the container
+         * @param value - value to initialize the container with
+         *
+         * @return none
+         *
+         */
+        void assign( size_type count, const T& value ) {
+            for (size_type i = 0; i < count; i++) {
+                _data[i] = value;
+            }
+        }
+
+        /*
+        template< class InputIt >
+        void assign( InputIt first, InputIt last ) {
+
+        }
          */
 
     /****************************Element access********************************/
+    //at
+    //operator[]
 
+    /**
+     * Returns reference to first element of container
+     *
+     * @returns reference to first element
+     */
     reference front(void) {
         return _data[0];
     }
 
+    /**
+     * Returns const reference to first element of container
+     *
+     * @returns const reference to first element
+     */
     const_reference front(void) const {
         return _data[0];
     }
 
+    /**
+     * Returns reference to last element of container
+     *
+     * @returns reference to last element
+     */
     reference back(void) {
         return _data[_size - 1];
     }
 
+    /**
+     * Returns reference to last element of container
+     *
+     * @returns reference to last element
+     */
     const_reference back(void) const {
         return _data[_size - 1];
     }
+
+    /**
+     * Returns a pointer to the underlying array serving as storage
+     *
+     * @param none
+     *
+     * @returns pointer to underlying element storage
+     */
+    pointer data( void ) {
+        return _data;
+    }
+
+    /**
+     * Returns a const pointer to the underlying array serving as storage
+     *
+     * @param none
+     *
+     * @returns const pointer to underlying element storage
+     */
+    const pointer data( void ) const {
+        return _data;
+    }
+
     /***************************Iterators**************************************/
     //begin
     //end
     //rbegin
     //rend
     /***************************Capacity***************************************/
-    //empty
-    //size 
-    //max_size 
+
+
+    /**
+     * Checks if container is empty, ie if begin() == end()
+     */
+    bool empty( void ) {
+        if (_size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns number of elements in the container
+     *
+     * @param  none
+     *
+     * @returns number of elements in the container
+     */
+    size_type size( void ) const {
+        return (_size);
+    }
 
     /**
      * Returns the maximum number of elements that the vector is able to hold 
@@ -118,26 +241,55 @@ template<class T,class Allocator = std::allocator<T> > class vector {
      * @returns Maximum number of elements 
      */
     size_type max_size(void) const {
-        return (_alloc.max_size());
+        return (_allocator.max_size());
     }
 
     /**
      * Requests that _capacity should at least contain new_cap elements.\n
      * If new_cap is greater than current vector _capacity, function causes 
-     * container to reallocate, increasing capacity to n.\n
+     * container to reallocate  increasing capacity to new_cap.\n
      * Complexity: if realloc happens, linear in vector_size at most\n
      *
      * @param new_cap - new capacity of the vector, in number of elements
+     *
      * @return none
+     *
      * @throws length_error - exception is size requested is greater than vector::max_size
-     * @throws bad_alloc -  if reallocation fails due to lack of space (when default alloc is used) 
+     * @throws bad_allocator -  if reallocation fails due to lack of space (when default alloc is used) 
      */
     void reserve(size_type new_cap) {
-
-
+        //TODO: write correct exception
+        if (new_cap > this->max_size()) {
+            throw std::exception();
+        }
+        //need to reallocate space
+        //TODO: probably better way to copy data over
+        //BUG: when constructing for the first time _data = 0x0
+        if (new_cap > _capacity) {
+            size_type oldCapacity = _capacity;
+            _capacity = new_cap;
+            pointer newData = _allocator.allocate(new_cap); //need power superior power of 2 of new capacity
+            for (size_type i = 0; i < _size; i++) {
+                _allocator.construct(&newData[i], _data[i]);
+            }
+            for (size_type i = 0; i < _size; i++) {
+                _allocator.destroy(&_data[i]);
+            }
+            _allocator.deallocate(_data, oldCapacity);
+            _data = newData;
+        }
     }
 
-    //capacity
+    /**
+     * Returns capacity of the container
+     *
+     * @param  none
+     *
+     * @returns capacity of the container
+     */
+    size_type capacity() const {
+        return (_capacity);
+    }
     /***************************Modifiers**************************************/
     //clear 
     //insert
--- Src/test.cpp
+++ Src/test.cpp
@@ -5,27 +5,13 @@
 
 int main (int argc, char *argv[])
 {
-    std::vector<int> v;
-    std::cout << "size "<< v.size() << std::endl;
-    std::cout << "capa " << v.capacity() << std::endl;
-    v.push_back(3);
-    std::cout << "size "<< v.size() << std::endl;
-    std::cout << "capa " << v.capacity() << std::endl;
-    v.push_back(4);
-    std::cout << "size "<< v.size() << std::endl;
-    std::cout << "capa " << v.capacity() << std::endl;
-    v.push_back(5);
-    std::cout << "size "<< v.size() << std::endl;
-    std::cout << "capa " << v.capacity() << std::endl;
-    std::cout << *(v.data()) << std::endl;
-    std::cout << v.front()<< std::endl;
-    std::cout << v.back()<< std::endl;
-    
+    /*
     int *v1 = &(v.front());
     *v1 = 2;
     std::cout << *v1 << std::endl;
     v.front() = 4; //peut assigner directment vu que v.front return un reference
     std::cout << v.front() << std::endl;
+    */
     /*
     for (size_t i = 0; i < 256;  i++) {
         v.push_back(i);
@@ -38,11 +24,24 @@ int main (int argc, char *argv[])
     }
     */
 
-    std::vector<int> v2; 
-    std::cout << v2.max_size() << std::endl;
+    std::vector<int> v2(1000, 2); 
+    std::cout << v2.capacity() << std::endl;
+    std::cout << v2.size() << std::endl;
+    v2.push_back(1);
+    std::cout << v2.capacity() << std::endl;
+    std::cout << v2.size() << std::endl;
 
     /////////////////////////////////
-    ft::vector<int> ownv2;
-    std::cout << ownv2.max_size() << std::endl;
+    ft::vector<int> vft2(1000, 2);
+    std::cout << vft2.capacity() << std::endl;
+    std::cout << vft2.size() << std::endl;
+    ft::vector<int> v3(2, 2);
+    ft::vector<int> v5(4, 2);
+    int *data = v2.data();
+    /*
+    for (std::size_t i = 0; i < ownv2.size(); i++) {
+        std::cout << data[i] << std::endl;
+    }
+    */
     return 0;
 }
